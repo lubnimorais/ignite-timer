8c25ce5a1d45312772b1530c9d6a2c8be6160a3d
Lubni Morais <lubni.morais@gmail.com>
2023-03-17 22:36:45 -0300

    feat: Added lib react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { ThemeProvider } from 'styled-components';
 
+import { BrowserRouter } from 'react-router-dom';
+
+import { Router } from './routes';
+
 import { defaultThemes } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
-
-import { Button } from './components/Button';
 
 function App() {
   return (
     <ThemeProvider theme={defaultThemes}>
       <GlobalStyle />
 
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="danger"></Button>
-      <Button variant="success"></Button>
-      <Button></Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
